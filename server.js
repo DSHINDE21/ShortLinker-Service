090e377548fca1e0137a6be7a1a6d0cff9bcd9f8
DSHINDE21 <dinesh.s.shinde1@gmail.com>
2024-07-29 23:14:14 +0530

    feat: mongo atlas and express app setup

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 import express from "express";
+import dotenv from "dotenv";
 
+//for reading env variables
+dotenv.config();
+
+// express app
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
+// middleware
 app.use(express.json());
 
 const startServer = () => {
   try {
+    // connect to db
+
+    // start & listen to the request
     app.listen(PORT, () => {
       console.log(`server started listing on PORT: ${PORT}`);
     });
   } catch (e) {
+    // error handling
     console.log(e);
   }
 };
